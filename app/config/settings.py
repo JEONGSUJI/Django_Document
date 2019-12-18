aaef2101b01e4ccd8cd9a42339e444944a5b63a8
JEONGSUJI <wtw9611@gmail.com>
2019-12-18 18:09:15 +0900

    many_to_one 실습

--- app/config/settings.py
+++ app/config/settings.py
@@ -32,12 +32,16 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'myapp.apps.MyappConfig',
+    'many_to_one.apps.ManyToOneConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
